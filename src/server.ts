6311200d3d8c5d3bbf5a6453c0c969d72f0db51a
Rafael <rafinha_fern@hotmail.com>
2022-08-09 20:24:49 -0300

    feat: to tests

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import app from "./app.js";
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is up and running on port 5000 :D :D`);
+  console.log(`:D :D Server is up and running on port 5000 :D :D`);
 })
